153ca7c1847c18a96a5157dd15c603cacb0da49f
nmesih <nmesihalan@gmail.com>
2022-10-09 10:25:07 +0300

    added listing 7 and 8

---
+++ src/Chapters/chapter_07/Listing_08_SelectionSort.java
@@ -0,0 +1,28 @@
+package Chapters.chapter_07;
+
+public class Listing_08_SelectionSort {
+    public static void main(String[] args) {
+        double[] list = {1, 9, 4.5, 6.6, 5.7, -4.5};
+        selectionSort(list);
+        for (int i = 0; i < list.length; i++) {
+            System.out.print(list[i] + " ");
+        }
+    }
+
+    public static void selectionSort(double[] list) {
+        for (int i = 0; i < list.length - 1; i++) {
+            double currentMin = list[i];
+            int currentMinIndex = i;
+            for (int j = i + 1; j < list.length; j++) {
+                if (currentMin > list[j]) {
+                    currentMin = list[j];
+                    currentMinIndex = j;
+                }
+            }
+            if (currentMinIndex != i) {
+                list[currentMinIndex] = list[i];
+                list[i] = currentMin;
+            }
+        }
+    }
+}
