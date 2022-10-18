c50cc7a456ef12cc0f05ab5127196ea927f61a9b
nmesih <nmesihalan@gmail.com>
2022-10-18 09:20:55 +0300

    Added checkpoints 5,6 and 7

---
+++ src/Chapters/chapter_08/Checkpoint_05.java
@@ -0,0 +1,12 @@
+package Chapters.chapters_08;
+
+public class Checkpoint_05 {
+    public static void main(String[] args) {
+        int[][] array = {{1, 2}, {3, 4}, {5, 6}};
+        for (int i = array.length - 1; i >= 0; i--) {
+            for (int j = array[i].length - 1; j >= 0; j--)
+                System.out.print(array[i][j] + " ");
+            System.out.println();
+        }
+    }
+}
---
+++ src/Chapters/chapter_08/Checkpoint_06.java
@@ -0,0 +1,11 @@
+package Chapters.chapters_08;
+
+public class Checkpoint_06 {
+    public static void main(String[] args) {
+        int[][] array = {{1, 2}, {3, 4}, {5, 6}};
+        int sum = 0;
+        for (int i = 0; i < array.length; i++)
+            sum += array[i][0];
+        System.out.println(sum);
+    }
+}
