c19d316c06805ec3841c74c7dd693f205bc851f7
nmesih <nmesihalan@gmail.com>
2022-10-30 16:56:10 +0300

    Added listing 3 and 4

---
+++ src/Chapters/Chapter_09/Listing_04_TestTV.java
@@ -0,0 +1,21 @@
+package Chapters.Chapter_09;
+
+public class Listing_04_TestTV {
+    public static void main(String[] args) {
+        Listing_03_TV tv1 = new Listing_03_TV();
+        tv1.turnOn();
+        tv1.setChannel(30);
+        tv1.setVolumeLevel(3);
+
+        Listing_03_TV tv2 = new Listing_03_TV();
+        tv2.turnOn();
+        tv2.channelUp();
+        tv2.channelUp();
+        tv2.volumeUp();
+
+        System.out.println("tv1's channel is " + tv1.channel +
+                " and volume level is " + tv1.volumeLevel);
+        System.out.println("tv2's channel is " + tv2.channel +
+                " and volume level is " + tv2.volumeLevel);
+    }
+    }
