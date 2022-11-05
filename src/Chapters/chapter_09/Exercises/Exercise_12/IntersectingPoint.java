181cac1a87f37e60594e45b9477f63d29c09b3aa
nmesih <nmesihalan@gmail.com>
2022-11-05 11:57:05 +0300

    Added exercise 12

---
+++ src/Chapters/chapter_09/Exercises/Exercise_12/IntersectingPoint.java
@@ -0,0 +1,38 @@
+package Chapters.chapter_09.Exercises.Exercise_12;
+
+import Chapters.chapter_09.Exercises.Exercise_11.LinearEquation;
+
+import java.util.Scanner;
+
+public class IntersectingPoint {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter x1, y1, x2, y2, x3, y3, x4, y4: ");
+        double x1 =input.nextDouble();
+        double y1 =input.nextDouble();
+        double x2 =input.nextDouble();
+        double y2 =input.nextDouble();
+        double x3 =input.nextDouble();
+        double y3 =input.nextDouble();
+        double x4 =input.nextDouble();
+        double y4 =input.nextDouble();
+
+        double a = y1 - y2;
+        double b = -(x1 - x2);
+        double e = (y1 - y2) * x1 - (x1 - x2) * y1;
+        double c = (y3 - y4);
+        double d = -(x3 - x4);
+        double f = (y3 - y4) * x3 - (x3 - x4) * y3;
+
+        LinearEquation solution = new LinearEquation(a,b,c,d,e,f);
+
+        if(solution.isSolvable()){
+            System.out.printf("The intersecting point is at (%.5f, %.5f)", solution.getX(), solution.getY());
+        }else
+            System.out.println("The two lines are parallel");
+    }
+    }
+
+
