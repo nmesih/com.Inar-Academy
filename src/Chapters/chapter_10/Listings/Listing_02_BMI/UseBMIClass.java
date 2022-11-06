75129f6d62511a63829698929cc55b0a2f3a618d
nmesih <nmesihalan@gmail.com>
2022-11-06 08:54:50 +0300

    added listings 1 and 2

---
+++ src/Chapters/chapter_10/Listings/Listing_02_BMI/UseBMIClass.java
@@ -0,0 +1,13 @@
+package Chapters.chapter_10.Listings.Listing_02_BMI;
+
+public class UseBMIClass {
+    public static void main(String[] args) {
+        BMI bmi1 = new BMI("Lionel Messi" , 34, 170, 70);
+        System.out.println("The BMI for " + bmi1.getName() + " is " +
+                bmi1.getBMI() + " " + bmi1.getStatus());
+
+        BMI bmi2 = new BMI("Cristiano Ronaldo" , 200, 80);
+        System.out.println("The BMI for " + bmi2.getName() + " is " +
+                bmi2.getBMI() + " " + bmi2.getStatus());
+    }
+}
