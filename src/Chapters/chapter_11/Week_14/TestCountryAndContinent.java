908b4fa992c69cc9430b2d91ac0723eae572b52a
nmesih <nmesihalan@gmail.com>
2022-11-13 13:37:27 +0300

    added practice in live session

---
+++ src/Chapters/chapter_11/Week_14/TestCountryAndContinent.java
@@ -0,0 +1,17 @@
+package Chapters.chapter_11.Week_14;
+
+public class TestCountryAndContinent {
+    public static void main(String[] args) {
+
+        Country turkiye = new Country("Turkiye", 783562, 83_000_000, "mediterranean climate",
+                "Turkce", new String[]{"Kebap", "Kunefe", "Lahmacun", "Doner", "Baklava"});
+
+        System.out.println(turkiye.toString());
+        turkiye.countryBreadth();
+        turkiye.republic(3);
+
+        System.out.println("------------");
+        Continent europe = new Continent("Europe", 10530000, 750_000_000, "Temperate climate", 64);
+        System.out.println(europe);
+    }
+}
