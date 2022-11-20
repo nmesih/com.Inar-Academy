20caac69c5c247e3d605dff363ea2f2f8faa10bb
nmesih <nmesihalan@gmail.com>
2022-11-20 21:39:29 +0300

    Added some listings

---
+++ src/Chapters/chapter_12/Listings/TestCircleWithException.java
@@ -0,0 +1,15 @@
+package Chapters.chapter_12.Listings;
+
+public class TestCircleWithException {
+    public static void main(String[] args) {
+        try {
+            CircleWithException c1 = new CircleWithException(5);
+            CircleWithException c2 = new CircleWithException(-5);
+            CircleWithException c3 = new CircleWithException(0);
+        } catch (IllegalArgumentException ex) {
+            System.out.println(ex);
+        }
+
+        System.out.println("Number of objects created: " + CircleWithException.getNumberOfObjects());
+    }
+}
