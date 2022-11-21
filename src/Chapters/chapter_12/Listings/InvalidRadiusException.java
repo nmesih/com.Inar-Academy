162541a7b577ed8228216b9eed0925ab4a79b096
nmesih <nmesihalan@gmail.com>
2022-11-21 22:25:44 +0300

    Added some listings

---
+++ src/Chapters/chapter_12/Listings/InvalidRadiusException.java
@@ -0,0 +1,15 @@
+package Chapters.chapter_12.Listings;
+
+public class InvalidRadiusException extends Exception {
+    private double radius;
+
+    public InvalidRadiusException(double radius) {
+        super("Invalid radius " + radius);
+        this.radius = radius;
+    }
+
+
+    public double getRadius() {
+        return radius;
+    }
+}
