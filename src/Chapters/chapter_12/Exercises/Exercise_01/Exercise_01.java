8557e9719bf5e8d8d9a3308eb5ea09bb0b7518c9
nmesih <nmesihalan@gmail.com>
2022-11-23 11:45:51 +0300

    Added exercise 1

---
+++ src/Chapters/chapter_12/Exercises/Exercise_01/Exercise_01.java
@@ -0,0 +1,34 @@
+package Chapters.chapter_12.Exercises.Exercise_01;
+
+public class Exercise_01 {
+    public static void main(String[] args) {
+
+        if (args.length != 3) {
+            System.out.println(
+                    "Usage: java Calculator operand1 operator operand2");
+            System.exit(0);
+        }
+        int result = 0;
+
+        try {
+
+        switch (args[1].charAt(0)) {
+            case '+':
+                result = Integer.parseInt(args[0]) + Integer.parseInt(args[2]);
+                break;
+            case '-':
+                result = Integer.parseInt(args[0]) - Integer.parseInt(args[2]);
+                break;
+            case '.':
+                result = Integer.parseInt(args[0]) * Integer.parseInt(args[2]);
+                break;
+            case '/':
+                result = Integer.parseInt(args[0]) / Integer.parseInt(args[2]);
+        }
+        System.out.println(args[0] + ' ' + args[1] + ' ' + args[2] + " = " + result);
+
+    } catch (Exception e) {
+            System.out.println("Wrong input: " + args[0] + args[1] + args[2]);
+        }
+    }
+}
